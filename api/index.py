6ffb21f4121a98d31de6fec2d046f1b5b3687a96
Marcos Tenorio <matenorio@gmail.com>
2024-09-19 11:52:44 -0300

    alter condif headers

--- api/index.py
+++ api/index.py
@@ -22,7 +22,7 @@ def rss_json():
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET',
         'Access-Control-Allow-Headers': 'Content-Type',
-    }*
+    }
 
     if request.method == 'OPTIONS':
         # Handle preflight requests
