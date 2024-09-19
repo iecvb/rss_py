60010f174908c49425aafe766df1ffb36b82329c
Marcos Tenorio <matenorio@gmail.com>
2024-09-19 13:41:27 -0300

    remove flask

--- api/index.py
+++ api/index.py
@@ -10,7 +10,6 @@ class RSSHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Busca e analisa os dados do podcast
         xml_text = fetch_podcast_data()
-
         podcast_data = parse_podcast_data(xml_text)
 
         # Retorna os dados do podcast em formato JSON
@@ -20,44 +19,44 @@ class RSSHandler(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(podcast_data).encode())
         return
 
-def rss_json():   
-    # Busca e analisa os dados do podcast
-    xml_text = fetch_podcast_data()
-    
-    if not xml_text:
-        return jsonify({'error': 'Erro ao buscar o feed do podcast'})
+    def rss_json():   
+        # Busca e analisa os dados do podcast
+        xml_text = fetch_podcast_data()
+        
+        if not xml_text:
+            return jsonify({'error': 'Erro ao buscar o feed do podcast'})
 
-    podcast_data = parse_podcast_data(xml_text)
-    
-    # Retorna os dados do podcast em formato JSON
-    return jsonify(podcast_data)
+        podcast_data = parse_podcast_data(xml_text)
+        
+        # Retorna os dados do podcast em formato JSON
+        return jsonify(podcast_data)
 
-def fetch_podcast_data():
-    try:
-        # Fazendo a requisição para o feed RSS
-        response = requests.get(PODCAST_URL)
-        response.raise_for_status()
-        return response.text
-    except requests.exceptions.RequestException as e:
-        print(f"Erro ao buscar o feed RSS: {e}")
-        return None
+    def fetch_podcast_data():
+        try:
+            # Fazendo a requisição para o feed RSS
+            response = requests.get(PODCAST_URL)
+            response.raise_for_status()
+            return response.text
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao buscar o feed RSS: {e}")
+            return None
 
-def parse_podcast_data(xml_text):
-    # Converte o XML em um dicionário usando xmltodict
-    podcast_dict = xmltodict.parse(xml_text)
-    items = podcast_dict['rss']['channel']['item']
+    def parse_podcast_data(xml_text):
+        # Converte o XML em um dicionário usando xmltodict
+        podcast_dict = xmltodict.parse(xml_text)
+        items = podcast_dict['rss']['channel']['item']
 
-    podcast_data = []
-    for item in items:
-        title = item['title']
-        enclosure_url = item['enclosure']['@url']
-        image_url = item.get('itunes:image', {}).get('@href', 'Imagem indisponível')
+        podcast_data = []
+        for item in items:
+            title = item['title']
+            enclosure_url = item['enclosure']['@url']
+            image_url = item.get('itunes:image', {}).get('@href', 'Imagem indisponível')
 
-        podcast_data.append({
-            'name': title,                        
-            'url': enclosure_url,
-            'cover_art_url': image_url
-        })
+            podcast_data.append({
+                'name': title,                        
+                'url': enclosure_url,
+                'cover_art_url': image_url
+            })
 
-    return podcast_data[:20]
+        return podcast_data[:20]
 
